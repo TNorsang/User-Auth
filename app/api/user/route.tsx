a042d59cec133eb1c18ad2579ca3c80ac0bcbd4b
Norsang Nyandak <TNorsang>
2024-05-24 15:57:01 -0400

    second commit

---
+++ app/api/user/route.tsx
@@ -0,0 +1,48 @@
+import { db } from "@/lib/db";
+import { NextResponse } from "next/server";
+import { hash } from "bcrypt";
+
+export async function POST(req: Request) {
+  try {
+    const body = await req.json();
+
+    const { email, username, password } = body;
+
+    const existingUserByEmail = await db.user.findUnique({
+      where: { email: email },
+    });
+    if (existingUserByEmail) {
+      return NextResponse.json(
+        { user: null, message: "Email already exists" },
+        { status: 409 }
+      );
+    }
+    const existingUserByUsername = await db.user.findUnique({
+      where: { username: username },
+    });
+    if (existingUserByUsername) {
+      return NextResponse.json(
+        { user: null, message: "Username already exists" },
+        { status: 409 }
+      );
+    }
+
+    const hashedPassword = await hash(password, 10);
+
+    const newUser = await db.user.create({
+      data: { email, username, password: hashedPassword },
+    });
+
+    const { password: newUserPassword, ...rest } = newUser;
+
+    return NextResponse.json(
+      { user: rest, message: "User created" },
+      { status: 201 }
+    );
+  } catch (error) {
+    return NextResponse.json(
+      { message: "Something went wrong" },
+      { status: 500 }
+    );
+  }
+}
